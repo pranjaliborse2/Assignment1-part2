3c9470f50b7152242027542879d72fb5793eb1ee
pranjaliborse2 <pranjaliborse21999@gmail.com>
2022-08-31 21:52:33 -0500

    modified script according to PEP8

--- addition.py
+++ addition.py
@@ -1,5 +1,13 @@
-a= int(input('enter a:'))
-b= int(input('enter b:'))
+a= int(
+    input('enter a:'))
+b= int(
+    input('enter b:'))
 s= a+b
-print('input is equal to'+str(a)+'and'+str(b))
-print('output is equal to'+str(s))
+print(
+    'input is equal to '
+     +str(a)
+     +' and '
+     +str(b))
+print(
+    'output is equal to '
+     +str(s))
