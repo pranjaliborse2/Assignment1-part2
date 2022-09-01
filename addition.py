a4986829dc1177779fa082a9d7e5805acda38d84
pranjaliborse2 <pranjaliborse21999@gmail.com>
2022-08-31 21:46:58 -0500

    testing the code

--- addition.py
+++ addition.py
@@ -1,5 +1,5 @@
 a= int(input('enter a:'))
 b= int(input('enter b:'))
-s=a+b
-print('input='+str(a)+','+str(b))
-print('output='+str(s))
+s= a+b
+print('input is equal to'+str(a)+'and'+str(b))
+print('output is equal to'+str(s))
