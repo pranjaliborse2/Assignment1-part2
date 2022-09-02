0923d5484d0faca33e12ca9ad398172a73ecf02e
pranjaliborse2 <pranjaliborse21999@gmail.com>
2022-09-01 20:50:15 -0500

    Modified script according to PEP257

--- addition.py
+++ addition.py
@@ -1,13 +1,22 @@
+"""Take two integers as input from user"""
 a= int(
     input('enter a:'))
+
 b= int(
     input('enter b:'))
+
+"""
+Perform addition on two inputs and
+print input and output
+"""
 s= a+b
+    
 print(
     'input is equal to '
      +str(a)
      +' and '
      +str(b))
+    
 print(
     'output is equal to '
      +str(s))
