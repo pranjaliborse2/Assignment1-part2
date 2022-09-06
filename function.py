3547a69a555ac747371ddb161983fd7204787a6c
pranjaliborse2 <pranjaliborse21999@gmail.com>
2022-09-05 22:39:00 -0500

    Created and runned new python file on new branch

---
+++ function.py
@@ -0,0 +1,22 @@
+"""Take two integers as input from user"""
+c= int(
+    input('enter a:'))
+
+d= int(
+    input('enter b:'))
+
+"""
+Perform multiplication on two inputs and
+print input and output
+"""
+m= c*d
+    
+print(
+    'input is equal to '
+     +str(c)
+     +' and '
+     +str(d))
+    
+print(
+    'Multiplication is '
+     +str(m))
